56f74e70e9f378916cf0e7f59aa5a8564e22bd23
Tina Dang <tdtinadang@gmail.com>
2025-02-13 01:00:32 -0800

    feat: Add function to randomly choose rock, paper, scissors

--- javascript.js
+++ javascript.js
@@ -1,1 +1,22 @@
-console.log("Hello World!");
+
+//Randomly return one of the following string values: “rock”, “paper” or “scissors”.
+function getComputerChoice() {
+    //Create a number variable to store random number
+    let randomNumber = Math.random();
+
+    if (randomNumber <= 0.33) {
+        //If the random number is less than or equal to 0.33, choose rock
+        return "rock";
+    } else if (randomNumber > 0.33 && randomNumber < 0.66) {
+        //If the random number is between 0.33 and 0.66, choose paper
+        return "paper";
+    } else if (randomNumber >= 0.66) {
+        //If the random number is greater than or equal to 0.66, choose scissors
+        return "scissors";
+    }
+
+}
+
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
